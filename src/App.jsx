3c8a89654d1122de5e8d16198b149f9dbe47c46c
farhanisty <farhannivtasija@gmail.com>
2024-12-30 23:01:17 +0700

    install react-router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Plan from "./pages/Plan.jsx";
+import Datasource from "./pages/Datasource.jsx";
 import SidebarLayout from "./layout/SidebarLayout.jsx";
 
 function App() {
-  return <SidebarLayout>hello world</SidebarLayout>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Plan />} />
+        <Route path="/datasource" element={<Datasource />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
