861f781cfc3043746d36bb42ecb8ae0394d2d714
farhanisty <farhannivtasija@gmail.com>
2025-01-01 15:44:06 +0700

    add accordion animation configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,32 @@ export default {
   				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
   				border: 'hsl(var(--sidebar-border))',
   				ring: 'hsl(var(--sidebar-ring))'
+  			},
+  			timeline: 'rgba(0,0,0,0.1)',
+  			timelabel: 'rgba(0,0,0,0.6)',
+  			subjectitem: 'rgba(0,125,255,0.2)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
   			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
